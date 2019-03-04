fa7a6c228ab555d8bad35729c8ac5b26f6892eef
HaoChuan9421 <hc199421@gmail.com>
2019-03-04 13:44:52 +0800

    不使用懒加载

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import Loadable from "react-loadable";
+// import Loadable from "react-loadable";
 
-const Layout1 = Loadable({
-  loader: () => import("./Layout1"),
-  loading() {
-    return <div>Loading...</div>;
-  }
-});
-const Layout2 = Loadable({
-  loader: () => import("./Layout2"),
-  loading() {
-    return <div>Loading...</div>;
-  }
-});
+// const Layout1 = Loadable({
+//   loader: () => import("./Layout1"),
+//   loading() {
+//     return <div>Loading...</div>;
+//   }
+// });
+// const Layout2 = Loadable({
+//   loader: () => import("./Layout2"),
+//   loading() {
+//     return <div>Loading...</div>;
+//   }
+// });
+import Layout1 from "./Layout1";
+import Layout2 from "./Layout2";
 
 class App extends Component {
   render() {
